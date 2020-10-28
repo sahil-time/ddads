e97c82b99def4afaf45b1ba8486e4ec16f26ffb8
Sahil Shah <sahilshah.time@gmail.com>
2020-10-29 02:29:31 +0530

    array util.c - all algo implementation

--- array_util.c
+++ array_util.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-#include "array_util_public.h"
+#include "array_util_private.h"
+#include "array_util_public.h" //Just to check Function Signatures given to Client against the Implementation Signatures
 
 /*
 	FORMAT TO IMPLEMENT ANY PUBLIC API EXCEPT INIT =>
@@ -11,11 +13,13 @@
 
 		> if(!self_assert(iada)) return;
 
-                > check if use_cache is True
+		> pthread_mutex_lock
+              
+		> check if use_cache is True
  
-                > Free Malloc memory before allocating again if any
+		> Free Malloc memory before allocating again if any
 
-		> ptr = malloc(required structures)
+		> ptr = calloc(required structures)
 		> if(ptr == NULL)...
 
 		...
@@ -23,18 +27,21 @@
 		...
 
 		> iada->is_max_cluster_run = true;
+
+		> pthread_mutex_unlock
 	}
 
 	SUGGESTIONS TO CREATE A PUBLIC API =>
 	1. Think about what happens if functions are called multiple times un-intentionally.
-	   > If you are mallocing memory and not freeing.
+	   > If you are callocing memory and not freeing.
 	   > Are they re-entrant [ do NOT work with per Thread data or Locks ]
 	   > Are they thread safe [ do NOT work on Global data, just on local data ]
 	   > Identify Critical regions where you need Locking [ Avoid if possible ]
 
 */
 
-/* - - - - - - - - - - INIT FUNCTION - - - - - - - - - -*/
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* - - - - - - - - - - INIT/DESTRUCT FUNCTION - - - - - - - - - -*/
 static bool
 self_assert(struct int_array_data_analysis* iada) {
 
@@ -51,6 +58,7 @@ self_assert(struct int_array_data_analysis* iada) {
    return true;
 }
 
+//Every INIT will create a New Object [ its like 'new Object()' in Java ]
 struct int_array_data_analysis*
 array_util_init(int const* data, const int size) {
 
@@ -64,7 +72,7 @@ array_util_init(int const* data, const int size) {
         return NULL;
     }
 
-    struct int_array_data_analysis* iada = (struct int_array_data_analysis*)malloc(sizeof(struct int_array_data_analysis)); //Init Structure Object
+    struct int_array_data_analysis* iada = (struct int_array_data_analysis*)calloc(1, sizeof(struct int_array_data_analysis)); //Init Structure Object
     if(iada == NULL) {
         perror("Error");
         return NULL;
@@ -74,11 +82,38 @@ array_util_init(int const* data, const int size) {
 
     iada->data = data;
     iada->size = size;
+
+    //Init Mutexes
+    pthread_mutex_init(&iada->lock_max_cluster, NULL);
+    pthread_mutex_init(&iada->lock_sorted_arr, NULL);
+    pthread_mutex_init(&iada->lock_stats, NULL);
+
     iada->init = true;
 
     return iada;
 }
 
+//Init-Destruct are NOT Synced. Can cause Producer-Consumer Problem [ Check in UT ]
+void
+array_util_destruct(struct int_array_data_analysis* iada) {
+
+    //Check if Malloc has been done ( We do NOT want to free random memory that might not be on Heap )
+    //Best is to check if iada->init  == true
+
+    if(iada == NULL) {
+        fprintf(stderr, "\nError: Object cannot be NULL");
+        return;
+    }
+
+    if(!iada->init) { //Can Seg-fault if iada is NOT-NULL but pointing to random memory as de-reference will fail
+        fprintf(stderr, "\nError: Object not initialized");
+        return;
+    }
+
+    free(iada);
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /* - - - - - - - - - - MAX CLUSTER FUNCTIONS - - - - - - - - - -*/
 static void
 array_util_print_max_cluster(struct int_array_max_cluster* clust) {
@@ -102,20 +137,22 @@ array_util_max_size_cluster(struct int_array_data_analysis* iada, bool use_cache
    
     if(!self_assert(iada)) return;
 
-    //Check if use_cache = False
+    pthread_mutex_lock(&iada->lock_max_cluster);
+ 
+    //Check if use_cache = True
     if(use_cache) {
         if(iada->is_max_cluster_run) {
             return;
         }
     }
     
-    //Before allocating free previous malloc [ or else leak ]
+    //Before allocating free previous calloc [ or else leak ]
     if(iada->max_cluster){
         free(iada->max_cluster);
     }
 
-    //Init Max Cluster [ Before you malloc again make sure you free it ]
-    iada->max_cluster = (struct int_array_max_cluster*)malloc(sizeof(struct int_array_max_cluster));
+    //Init Max Cluster [ Before you calloc again make sure you free it ]
+    iada->max_cluster = (struct int_array_max_cluster*)calloc(1, sizeof(struct int_array_max_cluster));
     if(iada->max_cluster == NULL) {
         perror("Error");
     }
@@ -126,7 +163,7 @@ array_util_max_size_cluster(struct int_array_data_analysis* iada, bool use_cache
     int* clust_end = &(iada->max_cluster->cluster_index_end);
     int const* array = iada->data;	//Point to actual data
     int size = iada->size;	//Create new Array because we DONT want to change existing Array [ also we have additional compile check - int const* ]
-    int res[size];    //Using VLA -> If input is large, consider malloc because Stack overflow.
+    int res[size];    //Using VLA -> If input is large, consider calloc because Stack overflow.
     res[0] = array[0];
     
     /* - - - - - - - - - - Kadane's algo Start - - - - - - - - - -*/
@@ -157,8 +194,11 @@ array_util_max_size_cluster(struct int_array_data_analysis* iada, bool use_cache
     *clust_start = index + 1; //off by 1
     
     iada->is_max_cluster_run = true;
+
+    pthread_mutex_unlock(&iada->lock_max_cluster);
 }
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 /* - - - - - - - - - - BASIC STATS RUN - - - - - - - - - -*/
 static void
 array_util_print_basic_stats(struct int_array_stats* stats) {
@@ -169,7 +209,56 @@ array_util_print_basic_stats(struct int_array_stats* stats) {
     }
 
     printf("\n");
-           
+    printf("\n\tArray Stats Mean: %d \
+           \n\tArray Stats Max Number: %d | Index: %d \
+           \n\tArray Stats Min Number: %d | Index: %d \
+           \n\tArray Stats Range: %lld", 
+           stats->mean,
+           stats->max_num, stats->max_num_index,
+           stats->min_num, stats->min_num_index,
+           stats->range);
+}
+
+static void
+array_util_basic_stats_calc_mean(struct int_array_data_analysis* iada) {
+
+    int const* data = iada->data;
+    long long sum = data[0];
+    int min = data[0];
+    int max = data[0];
+    int min_index = 0;
+    int max_index = 0;
+
+    for(int i = 1; i < iada->size; i++) {
+        sum = sum + data[i];
+
+        //Check min
+        if(min > data[i]) {
+            min = data[i];
+            min_index = i;
+        }
+
+        //Check max
+        if(max < data[i]) {
+            max = data[i];
+            max_index = i;
+        }
+    }
+
+    iada->stats->mean = sum/iada->size;
+    iada->stats->max_num = max;
+    iada->stats->max_num_index = max_index;
+    iada->stats->min_num = min;
+    iada->stats->min_num_index = min_index;
+    iada->stats->range = (long long)max - (long long)min;
+}
+
+static void
+array_util_basic_stats_calc_median_mode(struct int_array_data_analysis* iada) {
+
+    //Check if Sorted or else run it
+
+
 }
 
 void
@@ -177,31 +266,44 @@ array_util_basic_stats(struct int_array_data_analysis* iada, bool use_cache) {
 
     if(!self_assert(iada)) return;
 
-    //Check if use_cache = False
+    pthread_mutex_lock(&iada->lock_stats);
+
+    //Check if use_cache = True
     if(use_cache) {
         if(iada->is_stats_run) {
             return;
         }
     }
     
-    //Before allocating free previous malloc [ or else leak ]
+    //Before allocating free previous calloc [ or else leak ] - But quick calls on this function on same IADA will SegFault
     if(iada->stats){
         free(iada->stats);
+    }
 
     //Init Array Stats
-    iada->stats = (struct int_array_stats*)malloc(sizeof(struct int_array_stats));
+    iada->stats = (struct int_array_stats*)calloc(1, sizeof(struct int_array_stats));
     if(iada->stats == NULL) {
         perror("Error");
     }
 
-       
-    
+    //Calculate Mean, min and max
+    array_util_basic_stats_calc_mean(iada);
+
+    /* - - - - - - Median needs Sorting, if Max_Num - Min_Num is relatively small try using
+       - - - - - - Non-Comparison Sort to get better Time & Space complexity.
+       - - - - - - Mode can use the same provided by Median. */
+
+    //Calculate Median & Mode
+    array_util_basic_stats_calc_median_mode(iada);
+
     iada->is_stats_run = true;
+
+    pthread_mutex_unlock(&iada->lock_stats);
 }
 
 
-/* - - - - - - - - - - SORTED ARRAY RUN - - - - - - - - - -*/
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 /* - - - - - - - - - - PRINT ALL ATTRIBUTES - - - - - - - - - -*/
 void
 array_util_print_all(struct int_array_data_analysis* iada) {
