174bcaec387d828b815439673ab23dcff4944e4a
Sahil Shah <sahilshah.time@gmail.com>
2020-10-29 02:29:31 +0530

    provide this to client

--- array_util_public.h
+++ array_util_public.h
@@ -1,18 +1,23 @@
 //Public Util functions to hide Implementation details of Algorithms
-//To expose structures define them, to keep them private define in .c of implementation file and just use Forward Declaration here
+//Only satellite strucutres are exposed. The real Object is in array_util_private.h
 
 #ifndef __INT_ARRAY_UTIL_PUBLIC_H__
 #define __INT_ARRAY_UTIL_PUBLIC_H__
 
 #include <stdbool.h>
 
-struct int_array_max_cluster {
+//For Client use - DO NOT CHANGE THESE STRUCTS - Only used for Typecasting
+struct client_int_array_max_cluster {
     int cluster_index_start;
     int cluster_index_end;
     int cluster_size;
 };
 
-struct int_array_stats {
+struct client_int_array_sort {
+    int* sorted_arr;
+};
+
+struct client_int_array_stats {
     int mean;
     int median;
     int mode;
@@ -20,29 +25,7 @@ struct int_array_stats {
     int max_num_index;
     int min_num;
     int min_num_index;
-    int range;
-};
-
-struct int_array_data_analysis {
-
-    struct int_array_data_analysis* obj_id; //Obj Memory Location
-
-    //Data to operate on - Provided by Client! Treated as const data by functions analysing it
-    int const* data;
-    int size;
-    bool init;
-
-    //Find max cluster
-    struct int_array_max_cluster* max_cluster;
-    bool is_max_cluster_run;
-  
-    //Sort the array
-    int* sorted_arr; //Pointer to sorted Array
-    bool is_sorted_arr_run;
-
-    //Basic Statistic
-    struct int_array_stats* stats;
-    bool is_stats_run;
+    long long range;
 };
 
 /*
@@ -52,6 +35,11 @@ struct int_array_data_analysis {
   Ideally for a single piece of data, we want to create One IADA Object
 */
 struct int_array_data_analysis* array_util_init(int const* array, const int size);
+
+/*
+  Destroying the Object
+*/
+void array_util_destruct(struct int_array_data_analysis* iada);
 
 /*
   Find the max size of cluster in a given array [ also called max sub array problem ]
