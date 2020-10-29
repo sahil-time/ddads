f35fb5bfefd5c76319b3f4c4e31d7a7b55d16936
Sahil Shah <sahilshah.time@gmail.com>
2020-10-30 01:41:04 +0530

    Client

--- main.c
+++ main.c
@@ -9,11 +9,13 @@ main(void)
 {
     struct int_array_data_analysis* iada = NULL;
     iada = array_util_init(DATA, DATA_SIZE); //Once init, object is stored in 'iada' then use functions to work on it
+   // array_util_destruct(iada);
+    
 
     //Do Util on Array for Data Analysis
     //int array[9] = {-2,1,-3,4,-1,2,1,-5,4};
     clock_t begin = clock();
-    array_util_max_size_cluster(iada, true);
+    array_util_max_size_cluster(iada, false);
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     printf("\nTime Taken for Max Cluster: %f\n", time_spent);
