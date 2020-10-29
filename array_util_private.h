9dff298c748b2908bc77bb16e9697f3efe077816
Sahil Shah <sahilshah.time@gmail.com>
2020-10-29 18:35:26 +0530

    for private use by server

--- array_util_private.h
+++ array_util_private.h
@@ -25,10 +25,6 @@ struct int_array_max_cluster {
     int cluster_size;
 };
 
-struct int_array_sort {
-    int* sorted_arr;
-};
-
 struct int_array_stats {
     int mean;
     int median;
@@ -38,6 +34,7 @@ struct int_array_stats {
     int min_num;
     int min_num_index;
     long long range;
+    int* sorted_arr;
 };
 
 struct int_array_data_analysis {
@@ -53,14 +50,9 @@ struct int_array_data_analysis {
     struct int_array_max_cluster* max_cluster;
     bool is_max_cluster_run;
     pthread_mutex_t lock_max_cluster;
-  
-    //Sort the array
-    struct int_array_sort* sorted_arr; //Pointer to sorted Array
-    bool is_sorted_arr_run;
-    pthread_mutex_t lock_sorted_arr;
 
     //Basic Statistics
-    struct int_array_stats* stats;
+    struct int_array_stats* stats; //Sorts array also coz Median/Mode need it
     bool is_stats_run;
     pthread_mutex_t lock_stats;
 };
