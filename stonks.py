7977d366c591e2f5ba563b3857a04ce467c66e26
sahil-time <sahilshah.time@gmail.com>
2021-04-23 03:21:01 -0700

    additional data analysis on the output

--- stonks.py
+++ stonks.py
@@ -1,10 +1,15 @@
 import sys
+import os
 import csv
+import heapq
 import datetime
 from datetime import date
 
 #Usage:
 #	python3 stonks.py m o f c 2500 AMZN.csv
+#	Download AMZN.csv etc. from https://finance.yahoo.com/quote/AMZN/history?period1=1609632000&period2=1619049600&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true
+
+GLOBAL_LIST = []
 
 class bcolors:
     HEADER = '\033[95m'
@@ -64,9 +69,23 @@ def pretty_print(day1, day2, date, diff):
     else:
         print(map_integer_to_weekday(day1) + " -> " + map_integer_to_weekday(day2) + " " + date + " = " + bcolors.OKBLUE + str(diff) + bcolors.ENDC + "\n")
 
+    file1 = open("STONKS_DATASET.h", "a")
+#Playing safe and trying to maximize loss
+    if(diff < 0):
+        diff = diff - 1
+    file1.write(str(int(diff)) + ",\n")
+    file1.close()
+
 ################################################################################
 
 with open(sys.argv[6], newline='') as csvfile:
+    print("\n")
+
+    try:
+        os.remove(os.getcwd() + "/STONKS_DATASET.h")
+    except:
+        print("...")
+
     reader = csv.DictReader(csvfile)
 
     day1 = map_day_to_integer(sys.argv[1]);
@@ -79,7 +98,11 @@ with open(sys.argv[6], newline='') as csvfile:
 
     gross = 0
 
-    print("\n\n")
+    file1 = open("STONKS_DATASET.h", "a")
+    file1.write("#ifndef __STONKS_DATASET_H__\n")
+    file1.write("#define __STONKS_DATASET_H__\n")
+    file1.write("\nconst int STONKS_DATA[] = {\n")
+    file1.close()
 
 #Calculate
     for row in reader:
@@ -93,6 +116,7 @@ with open(sys.argv[6], newline='') as csvfile:
         if(date_time_obj.weekday() == day2):
             day2_price = row[day2_time]
             diff = float(day2_price) - float(day1_price)
+            GLOBAL_LIST.append(diff)	#Do data analysis on this list
 
             if(diff < 0):
                 gross = gross - options_val
@@ -101,4 +125,63 @@ with open(sys.argv[6], newline='') as csvfile:
 
             pretty_print(day1, day2, date_time_str, diff)
 
+    #Footers in the file
+    file1 = open("STONKS_DATASET.h", "a")
+    file1.write("};")
+    file1.write("\n\n#endif")
+    file1.close()
+
     print("\nNET GROSS: " + str(gross) + "\n")
+
+################################################################################
+
+#DATA ANALYSIS ON THE LIST
+
+################################################################################
+
+total_pos = 0
+total_neg = 0
+max_contig_pos = 0
+max_contig_neg = 0
+
+if GLOBAL_LIST[0] < 0:
+    contig_pos = 0
+else:
+    contig_pos = 1
+
+tmp = 0
+
+for val in GLOBAL_LIST:
+    if val < 0:
+        #Logic to calculate max contiguous surges and plummets
+        if(contig_pos == 1):
+            if(max_contig_pos < tmp):
+                max_contig_pos = tmp
+            contig_pos = 0
+            tmp = 0
+
+        total_neg += 1
+        tmp  += 1
+    else:
+        #Logic to calculate max contiguous surges and plummets
+        if(contig_pos == 0):
+            if(max_contig_neg < tmp):
+                max_contig_neg = tmp
+            contig_pos = 1
+            tmp = 0
+
+        total_pos += 1
+        tmp += 1
+
+print("Total Surges: " + str(total_pos) + "\nTotal Plummets: " + str(total_neg) + "\nMax Continuous Surges: " + str(max_contig_pos) + "\nMax Continuous Plummets: " + str(max_contig_neg))
+print("\n")
+
+################# HEAP EXPERIMENT #################
+li = [9, 5, 4, 3, 2, 1]
+heapq.heapify(li)
+print(li)
+heapq.heappush(li, 0)
+print(li)
+heapq.heapreplace(li, -1)
+print(heapq.nlargest(3,li))
+print(li)
